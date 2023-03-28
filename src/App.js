04ae761e6a74e06a368adb78b22a1eeb45f334cf
rohitmauryax <rohitmaurya152@gmail.com>
2023-03-28 21:55:15 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Charts from "./components/chart1";
+import Chart from "./components/chart2";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="box">
+        <div className="leftpannel">
+          <div className="charts">
+            <Charts />
+          </div>
+          <div className="chart-data">
+            <table>
+              <tr>
+                <th>WAVE</th>
+                <td>
+                  <input placeholder="SINE"></input>
+                </td>
+              </tr>
+              <tr>
+                <th>FREQ</th>
+                <td>
+                  <input placeholder="01000000.000000HZ"></input>
+                </td>
+              </tr>
+              <tr>
+                <th>AMPL</th>
+                <td>
+                  <input placeholder="05.000V"></input>
+                </td>
+              </tr>
+              <tr>
+                <th>OFFSET</th>
+                <td>
+                  <input placeholder="00.000V"></input>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div className="chart-div-3">
+            <table>
+              <tr>
+                <th>PHASE</th>
+                <th>
+                  <input placeholder="000 degree" />
+                </th>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="rightpannel">
+          <div className="charts">
+            <Chart />
+          </div>
+          <div className="chart-data">
+            <table>
+              <tr>
+                <th>WAVE</th>
+                <td>
+                  <input placeholder="SINE"></input>
+                </td>
+              </tr>
+              <tr>
+                <th>FREQ</th>
+                <td>
+                  <input placeholder="01000000.000000HZ"></input>
+                </td>
+              </tr>
+              <tr>
+                <th>AMPL</th>
+                <td>
+                  <input placeholder="05.000V"></input>
+                </td>
+              </tr>
+              <tr>
+                <th>OFFSET</th>
+                <td>
+                  <input placeholder="00.000V"></input>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div className="chart-div-3">
+            <table>
+              <tr>
+                <th>PHASE</th>
+                <th>
+                  <input placeholder="000 degree" />
+                </th>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
